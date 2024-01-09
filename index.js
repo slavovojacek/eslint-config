ec810cb578ff4cc04c9db6df35433fa0c3ccd49e
Slavo Vojacek <public@slavovojacek.com>
2024-01-09 15:16:56 +0000

    Update plugin and config

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  env: { es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
   ],
+  ignorePatterns: [],
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', 'simple-import-sort', 'unused-imports'],
   rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    'prettier/prettier': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        argsIgnorePattern: "^_",
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
       },
     ],
+    'unused-imports/no-unused-imports': 'error',
   },
 };
